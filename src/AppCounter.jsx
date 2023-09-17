110ce1b6eb63597f89000c6a8b6399f0959c4aef
AhnYeonghoo <dksdudgn00@naver.com>
2023-09-17 10:58:31 +0900

    Avatar Components setState

---
+++ src/AppCounter.jsx
@@ -0,0 +1,11 @@
+import Counter from './components/Counter';
+import './App.css';
+import React from 'react';
+
+export default function AppCounter() {
+	return (
+		<div className='container'>
+			<Counter />
+		</div>
+	);
+}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
+import Avatar from './Avatar';
+
 export default function Profile({ image, name, title, isNew }) {
 	return (
 		<div className='profile'>
-			<img className='photo' src={image} alt='avatar' />
-			{isNew && <span className='new'>new</span>}
+			<Avatar image={image} isNew={isNew} />
 			<h1>{name}</h1>
 			<p>{title}</p>
 		</div>
