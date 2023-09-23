b06e8a6e1f67421ba6909a7ab5b929b24c5259ed
AhnYeonghoo <dksdudgn00@naver.com>
2023-09-23 11:03:43 +0900

    useEffect

--- src/AppCounter.jsx
+++ src/AppCounter.jsx
@@ -1,11 +1,21 @@
 import Counter from './components/Counter';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AppCounter() {
+	const [count, setCount] = useState(0);
+	const handleClick = () => {
+		setCount((prev) => prev + 1);
+	};
 	return (
 		<div className='container'>
-			<Counter />
+			<div className='banner'>
+				Total Count: {count} {count > 10 ? 'fire' : 'flower'}
+			</div>
+			<div className='counter'>
+				<Counter total={count} onClick={handleClick} />
+				<Counter total={count} onClick={handleClick} />
+			</div>
 		</div>
 	);
 }
